2c87cd00d5891cd544b9d27a0b8a13c3fbfed8fb
Dunya-coder <diloromabdusattorova90@gmail.com>
2024-11-26 07:52:14 +0500

    INFRA:Upload project

---
+++ Linq_operatorlar/Linq_operatorlar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+class GroupMisol
+{
+    static void Main()
+    {
+        string[] websites = { "webNameA.com", "webNameB.net",
+                      "webNameC.net",
+                      "webNameF.org",
+                      "webNameI.tv",
+                      "webNameD.com",
+                      "webNameE.org",
+                      "webNameG.tv",
+                       "webNameH.net",
+                            };
+        var webAddrs = from addr in websites
+                       where addr.LastIndexOf('.') != -1
+                       group addr by
+                       addr.Substring(addr.LastIndexOf('.')) into ws
+                       where ws.Count() > 2
+                       select ws;
+        // So'rovni bajarish va uning natijalarini ko'rish. 
+        foreach (var sites in webAddrs)
+        {
+            Console.WriteLine(sites.Key + " domenidagi web saytlar:");
+            foreach (var site in sites)
+                Console.WriteLine(" " + site);
+            Console.WriteLine();
+        }
+    }
+}
